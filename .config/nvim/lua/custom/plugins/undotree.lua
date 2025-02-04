25c6219a28ffb7acc7d46671d0e04dd402984430
Lucas Alvin Lehmann <141682403+LucasLehmann@users.noreply.github.com>
2025-02-04 08:52:13 +0100

    nvim

--- .config/nvim/lua/custom/plugins/undotree.lua
+++ .config/nvim/lua/custom/plugins/undotree.lua
@@ -2,10 +2,8 @@ return {
   {
     'mbbill/undotree',
     config = function()
-      if vim.fn.has 'win32' or vim.fn.has 'win64' then
+      if vim.fn.has 'win32' == 1 or vim.fn.has 'win64' == 1 then
         vim.g.undotree_DiffCommand = 'FC'
-      elseif vim.fn.has 'unix' then
-        vim.g.undotree_DiffCommand = 'diff'
       end
       vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Toggle Undotree' })
     end,
